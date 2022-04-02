b2a0f0372cd9b5b79054c03ce16d75b04a3b6ccd
khanshorif331 <khanshorif331@gmail.com>
2022-04-02 20:05:18 +0600

    added some conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,15 +3,37 @@ import './Cart.css'
 
 const Cart = ({cart,handleRemoveFromCart}) => {
     // conditional renderring options
-    
+    // 1. Element variable;
+    // 2.ternary operator
+    // 3.&& operator
+    // 4.|| operator
+
+    let command;
+    if(cart.length === 0){
+        command = <p>Please add atleast one item</p>
+    }
+    else if(cart.length === 1){
+        command = <p>Please add more</p>
+    }
+    else{
+        command = <p><small>Thanks for addding item.</small></p>
+    }
     return (
         <div>
             <h2>Items Selected: {cart.length}</h2>
+            
             {
                 cart.map(tshirt=><p>{tshirt.name}
                 <button onClick={()=>handleRemoveFromCart(tshirt)}>X</button>
                 </p>)
             }
+            {cart.length===3 && <div className='orange'>
+                <h3>Trigonal</h3>
+                <p>3 jon k ki gift diba</p>
+                </div>}
+            {command} 
+            {cart.length !==4 ? <p>Keep adding</p> : <button>Clear All</button>}
+            {cart.length ===4 && <button className='orange'>Review Order</button>}
         </div>
     );
 };
